8266ceb43ca4f02de3d50787ab5fc249be540436
Mauff Sarah <sm236@hdm-stuttgart.de>
2021-11-04 16:49:19 +0100

    initial commit

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -9,21 +9,28 @@ export const Menu: React.FunctionComponent = () => {
     const actions = useActions().menu
     
     const dishes = state.menu.dishes.map(dish => (
-    <div>
-        <div>
+    <div className="border border-solid ">
+        <div className="">
             {dish.name}
         </div>
-        <div>
+        <div className="">
             {dish.price}
         </div>
     </div>
     ))
     
-    return (
-        <div>
-        <button onClick={() => actions.nameChanger( "Wert")}>changename</button>
-        <h3 className="text-lg font-bold">{state.name}Menu</h3>
-        <div>{dishes}</div>
+    return ( 
+        <div className="h-screen flex grid grid-rows-6 grid-cols-1 border-solid table-auto">
+            <div>
+                <div className="row-span-1 flex-auto overflow-hidden table-row">Menu</div>
+                <div className="flex-auto overflow-hidden table-row">Search</div>
+                <div className="flex-auto overflow-hidden table-row">Categories</div>
+            </div>
+            <div className="row-span-4 flex-auto overflow-y-auto table-row">{dishes} {dishes} {dishes} {dishes} {dishes}</div>
+            <div className="grid grid-cols-2 flex-auto flex-grow overflow-hidden table-row">
+                <button className="bg-gray-300 h-auto flex-auto">Bestellung anzeigen</button>
+                <button className="bg-gray-300 flex-auto">Für XXX bestellen</button>
+                </div>
         </div>
         
     )
