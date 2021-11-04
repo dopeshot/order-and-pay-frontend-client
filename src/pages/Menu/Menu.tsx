212de71da2c6fcbfef5a8eea1013cd010e1f2996
Mauff Sarah <sm236@hdm-stuttgart.de>
2021-11-04 23:11:59 +0100

    finish lower buttons

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -10,43 +10,41 @@ export const Menu: React.FunctionComponent = () => {
     const state = useAppState().menu
     const actions = useActions().menu
 
-    
+
     const dishes = state.menu.dishes.map(dish => (
-    <div className="border border-solid ">
-        <div className="">
-            {dish.name}
+        <div className="border border-solid ">
+            <div className="">
+                {dish.name}
+            </div>
+            <div className="">
+                {dish.price}
+            </div>
         </div>
-        <div className="">
-            {dish.price}
-        </div>
-    </div>
     ))
 
     const categories = state.menu.categories.map(category => (
-       <div>
-           
+        <div>
+
             <div className="shadow-md rounded-md overflow-hidden text-xl b-2">
-                 {category.name}
+                {category.name}
             </div>
         </div>
-      ))
-    
+    ))
 
-    return ( 
+
+    return (
         <div className="h-screen flex grid grid-rows-6 grid-cols-1 border-solid table-auto">
-            <div>
+            <div className="grid grid-rows-3">
                 <div className="row-span-1 flex-auto overflow-hidden table-row">Menu</div>
                 <div className="flex-auto overflow-hidden table-row">Search</div>
                 <div className="flex-auto overflow-hidden table-row">Categories</div>
             </div>
             <div className="row-span-4 flex-auto overflow-y-auto table-row">{dishes} {dishes} {dishes} {dishes} {dishes}</div>
-            <div className="grid grid-cols-2 flex-auto flex-grow overflow-hidden table-row">
-                <button className="bg-gray-300 h-auto flex-auto">Bestellung anzeigen</button>
-                <button className="bg-gray-300 flex-auto">Für XXX bestellen</button>
-                </div>
-
-   
+            <div className="grid grid-cols-2 flex-auto flex-grow overflow-hidden table-row place-content-evenly">
+                <div className="flex flex-auto place-self-center"><button className="menuButton h-auto" id="leftButton">Bestellung anzeigen</button></div>
+                <div className="flex flex-auto place-self-center"><button className="menuButton h-auto" id="rightButton">Für XXX bestellen</button></div>
+            </div>
         </div>
-        
+
     )
 }
