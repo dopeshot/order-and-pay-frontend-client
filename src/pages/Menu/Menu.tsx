31148fe5143c5775bc43d1e68583dd38cd93e432
Mauff Sarah <sm236@hdm-stuttgart.de>
2021-11-05 21:16:09 +0100

    delete content of index css

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -14,13 +14,13 @@ export const Menu: React.FunctionComponent = () => {
 
 
     const dishes = state.menu.dishes.map(dish => (
-    <div className="">
-       
-    </div>
-    
+        <div className="">
+
+        </div>
+
     ))
 
-    const categories = state.menu.categories.map(category =>(
+    const categories = state.menu.categories.map(category => (
         <div>
             {category.name}
             {dishIndexMap(category)}
@@ -28,36 +28,39 @@ export const Menu: React.FunctionComponent = () => {
 
     ))
 
-    function dishIndexMap(category: any){
+    function dishIndexMap(category: any) {
         //@ts-ignore
-        const dishes = category.dishIndex.map(index =>(
-        <div className="block p-2">
-            <DishCard name = {state.menu.dishes[index].name}  price = {state.menu.dishes[index].price}/>
-        </div>
+        const dishes = category.dishIndex.map(index => (
+            <div className="block p-2">
+                <DishCard name={state.menu.dishes[index].name} price={state.menu.dishes[index].price} />
+            </div>
         ))
         return dishes
     }
 
 
     return (
-        <div className="h-screen flex grid grid-rows-10 grid-cols-1 border-solid table-auto">
+        <div className="h-screen flex grid grid-rows-7 grid-cols-1 border-solid table-auto">
             <div className="grid grid-rows-6">
                 <div className="row-span-2 grid grid-cols-2 flex-auto overflow-hidden table-row">
                     <div className="pt-14 text-4xl pl-2 font-semibold" >Menü</div>
                     <button className="pb-8 pr-3 text-4xl pl-2 text-gray-600 text-right">=</button>
                 </div>
-                <div className="pt-4 row-span-1 flex justify-center"><Searchbar/></div>
+                <div className="pt-4 row-span-1 flex justify-center"><Searchbar /></div>
                 <div className="grid grid-cols-2">
                     <div className="pt-4 text-2xl pl-2 font-semibold">Kategorien</div>
                     <div className="pt-7 text-sm text-right pr-5 text-green-400 font-bold">Alle Anzeigen</div>
                 </div>
-                         
-                <div className=" row-span-3 flex-auto overflow-hidden table-row"> <Categories/> </div>
+
+                <div className=" row-span-3 flex-auto overflow-hidden table-row"> <Categories /> </div>
             </div>
-            <div className="p-4 row-span-4 flex-auto overflow-y-auto table-row">{categories}</div>
-            <div className="grid grid-cols-2 flex-auto flex-grow overflow-hidden table-row place-content-evenly">
-                <div className="flex flex-auto place-self-center"><button className="menuButton h-auto" id="leftButton">Bestellung anzeigen</button></div>
-                <div className="flex flex-auto place-self-center"><button className="menuButton h-auto" id="rightButton">Für zu viel geld bestellen</button></div>
+            <div className="p-4 flex-auto overflow-y-auto table-row">{categories}</div>
+            <div className="row-span-1 grid grid-cols-2 table-row place-content-evenly flex flex-auto place-self-center cursor-pointer">
+
+                <button className="menuButton h-auto rounded-lg text-green-400 text-center text-sm border border-solid border-green-400 py-4 px-8 my-4 mx-2">Bestellung anzeigen</button>
+
+                <button className="menuButton h-auto bg-green-400 text-white rounded-lg text-center text-sm py-4 px-8 my-4 mx-2">Für zu viel geld bestellen</button>
+
             </div>
         </div>
 
