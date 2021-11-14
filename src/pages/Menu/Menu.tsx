7111aef07e762401cac075404d52ab23389e5775
Heuserus <ben.haussermann@gmail.com>
2021-11-15 00:43:55 +0100

    move order button to own component

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Searchbar } from '../../components/MenuComponents/Searchbar';
 import { Categories } from '../../components/MenuComponents/Categories';
 import { MenuComponent } from '../../components/MenuComponents/MenuComponent';
+import { OrderButton } from '../../components/MenuComponents/OrderButton';
 import { Link } from "react-router-dom"
 import { useRef } from 'react';
 import { useAppState } from '../../overmind';
@@ -31,15 +32,7 @@ export const Menu: React.FunctionComponent = () => {
                 <div className=" row-span-3 flex-auto overflow-hidden table-row"> <Categories /> </div>
             </div>
             <div className="flex-auto overflow-y-auto table-row"><MenuComponent/></div>
-                <button className="menuButton bg-red bg-cover text-white h-16 grid grid-cols-5 flex items-center shadow-2xl" style={{boxShadow: "0px 10px 25px 10px rgb(245,85,85)"}}>
-                    <div className="flex justify-center">
-                    <p className="cols-span-1 text-red bg-white rounded-full pb-1 font-semibold w-7 h-7 flex items-center justify-center text-center">2</p>
-                    </div>
-                    <p className="col-start-2 col-span-3 font-semibold">Warenkorb anzeigen</p>
-                    <div className="flex justify-center">
-                    <p className="text-right font-semibold cols-span-1">5,60 €</p>
-                    </div>
-                </button>
+                <OrderButton/>
         </div>
 
     )
