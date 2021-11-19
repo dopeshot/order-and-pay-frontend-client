cdaeec88d57c95076bbf12d671c712ae297ce48c
Heuser <bh070@hdm-stuttgart.de>
2021-11-20 00:19:45 +0100

    update tests

--- cypress/integration/Pages/Menu.ts
+++ cypress/integration/Pages/Menu.ts
@@ -1,21 +1,45 @@
 
-
 import cypress from "cypress"
+
+
+const baseUrl = Cypress.config().baseUrl
+before(() => {
+    cy.visit("/menu")
+})
 
 describe("Renders homepage", () => {
     it("Renders correctly", () => {
-        cy.visit("/menu");
-        cy.get("#page").should("exist")
-        cy.get("#searchbar").should("exist")
-        cy.get("#categories").should("exist")
-        cy.get("#menuComponent").should("exist")
-        cy.get("#orderButton").should("exist")
+        cy.get("#page").should("be.visible")
+        cy.get("#searchbar").should("be.visible")
+        cy.get("#categories").should("be.visible")
+        cy.get("#menuComponent").should("be.visible")
+        cy.get("#orderButton").should("be.visible")
     })
     it("Opens the 'Alles Anzeigen' page ", () => {
-        cy.visit("/menu")
         cy.get('#showAll').click();
-        cy.url().should('eq', 'http://localhost:3000/categories')
+        cy.url().should('eq',baseUrl+ '/categories')
         cy.visit("/menu")
+    })
+
+  
+
+    it("Button Scolls work", function() {
+
+        cy.get("#categoryButton_2").click()
+        cy.get('#categoryButton_2').invoke('text').as('button2').then(() =>{
+            cy.get("#menuComponent").contains(this.button2).should("be.visible")
+        })
+        
+
+        cy.get("#categoryButton_0").click()
+        cy.get('#categoryButton_0').invoke('text').as('button0').then(() =>{
+            cy.get("#menuComponent").contains(this.button0).should("be.visible")
+        })
+        
+        
+        
+            
+        
     })
 })
   
