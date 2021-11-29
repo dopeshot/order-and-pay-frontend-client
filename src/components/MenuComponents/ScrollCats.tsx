9bdffd8ee52615db5bc7ecc24d5e98f6efc65869
Lars Flieger <a.purenetwork@gmail.com>
2021-11-29 19:46:33 +0100

    Add scroll to in small cats

--- src/components/MenuComponents/ScrollCats.tsx
+++ src/components/MenuComponents/ScrollCats.tsx
@@ -1,14 +1,12 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useAppState } from '../../overmind'
-import { scroller } from "react-scroll";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 //import { scrollTo } from '../../services/utilities'
-import Scrollspy from 'react-scrollspy'
-import { useEffect } from 'react';
+import Scrollspy from 'react-scrollspy';
+import { useAppState } from '../../overmind';
+
 
 
 type PropTypes = {
-    scrollFC : (id: number) => void
+    scrollFC: (id: number) => void
 }
 
 
@@ -18,47 +16,46 @@ export const ScrollCats: React.FunctionComponent<PropTypes> = (props: PropTypes)
     const state = useAppState().menu
 
     // One Category
-    const categories = state.menu.categories.map((category,index) => (
-        
-           
-        <button key={category._id + "_scrollButton" +index} id={"categoryScroll_" + index} className="text-red font-sofia font-bold text-center m-1 h-10 w-20 shadow-md rounded-md overflow-hidden text-xs b-2 focus:bg-red focus:text-white" onClick={() => props.scrollFC(index)}  >
+    const categories = state.menu.categories.map((category, index) => (
+
+
+        <button key={category._id + "_scrollButton" + index} id={"categoryScroll_" + index} className="text-red font-sofia font-bold text-center m-1 h-10 w-20 shadow-md rounded-md overflow-hidden text-xs b-2 focus:bg-red focus:text-white" onClick={() => props.scrollFC(index)}  >
             <div className="text-2xl ">
             </div>
             {category.name}
         </button>
-        
-        
+
+
     ))
 
-    const sections : string[] = []
-    state.menu.categories.forEach((category,index) => {
-        sections.push("section-"+(index+1))
+    const sections: string[] = []
+    state.menu.categories.forEach((category, index) => {
+        sections.push("section-" + (index + 1))
     });
 
     const scrollToButton = () => {
-        console.log("scrolling")
-        if(document.getElementsByClassName("bg-blue-700")[0]){
-            console.log(document.getElementsByClassName("bg-blue-700")[0])
-            document.getElementsByClassName("bg-blue-700")[0].scrollIntoView({
-                behavior: 'smooth',
-                inline: 'center'
-              })
-            }
-        }
+        const activeElements = document.querySelector(".bg-blue-700")!
+        const header = document.querySelector('#menu-header')!
+        const scrollSpy = document.querySelector('.scrollspy')!
+
+        header.scrollTo({
+            left: activeElements.getBoundingClientRect().left - scrollSpy.getBoundingClientRect().left,
+            behavior: 'smooth'
+        })
+    }
 
     return (
-        <header  className='grid grid-flow-col auto-cols-max md:auto-cols-min flex flex-row gap-1 overflow-auto'>
-        <FontAwesomeIcon icon="search" />
-    <Scrollspy
-        className="scrollspy grid grid-flow-col auto-cols-max md:auto-cols-min flex flex-row gap-1 overflow-auto"
-        items={sections}
-        currentClassName="bg-blue-700"
-        onUpdate={() => scrollToButton()}
-        offset={-50}
-        
-      >  
-            {categories}
-      </Scrollspy>
-      </header>)
-    
+        <header id="menu-header" className='grid grid-flow-col auto-cols-max md:auto-cols-min flex flex-row gap-0 overflow-auto'>
+            {/* <FontAwesomeIcon icon="search" /> */}
+            <Scrollspy
+                className="scrollspy grid grid-flow-col auto-cols-max md:auto-cols-min flex flex-row gap-1 overflow-auto"
+                items={sections}
+                currentClassName="bg-blue-700"
+                onUpdate={() => scrollToButton()}
+                offset={-50}
+            >
+                {categories}
+            </Scrollspy>
+        </header>)
+
 }
