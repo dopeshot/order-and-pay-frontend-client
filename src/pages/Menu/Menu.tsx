dda85aee55cd72771c9cfab849614d78ee55a121
leadannecker <leadannecker@googlemail.com>
2021-11-30 20:10:39 +0100

    Changed with from the MenuComponents

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -11,7 +11,6 @@ import { useScrollToNav } from '../../hooks/useScroll';
 
 
 export const Menu: React.FunctionComponent = () => {
-   
 
     const [containerRef, isVisible] = useScrollToNav({
         root: null,
@@ -20,31 +19,22 @@ export const Menu: React.FunctionComponent = () => {
 
     const MenuRef = useRef()
 
-    const scroll = (index : number) => {
+    const scroll = (index: number) => {
         //@ts-ignore
         MenuRef.current.handleClick(index)
     }
 
-    
-
-   
     return (
 
-        <div  id="page" className="container flex grid grid-rows-7 grid-cols-1 border-solid table-auto">
-            
-            
-      
-
-        <div className="sticky top-0 bg-white">{isVisible ? null : <ScrollCats scrollFC={scroll} />}</div>
-            <div>
-                {/*@ts-ignore */ }
+        <div id="page" className="flex grid grid-rows-7 grid-cols-1 border-solid table-auto w-full">
+            <div className="sticky top-0 bg-white">{isVisible ? null : <ScrollCats scrollFC={scroll} />}</div>
+            <div className= "w-full">
+                {/*@ts-ignore */}
                 <div ref={containerRef} ><Head scrollFC={scroll} /></div>
-                <div id="menuComponent" className="flex-auto overflow-y-auto pb-96" ><MenuComponent ref={MenuRef}/></div>
+                <div id="menuComponent" className="flex-auto overflow-y-auto pb-96 w-full" ><MenuComponent ref={MenuRef} /></div>
             </div>
-            
-            
-            <footer onClick={() => {}}  className="flex-grow fixed bottom-0 w-full"><OrderButton /></footer>
-
+            <footer onClick={() => { }} className="flex-grow fixed bottom-0 w-full"><OrderButton /></footer>
         </div>
-    )}
+    )
+}
 
