bae42de1adba1bda31a51b88e31385dfb77c9e4d
Mauff Sarah <sm236@hdm-stuttgart.de>
2021-11-30 22:14:31 +0100

    add transition setup

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,17 +1,13 @@
-import React, { useRef } from 'react';
+import { Transition } from '@headlessui/react';
+import React, { Fragment, useRef } from 'react';
 import { Head } from '../../components/MenuComponents/Head';
 import MenuComponent from '../../components/MenuComponents/MenuComponent';
 import { OrderButton } from '../../components/MenuComponents/OrderButton';
 import { ScrollCats } from '../../components/MenuComponents/ScrollCats';
 import { useScrollToNav } from '../../hooks/useScroll';
 
-
-
-
-
-
 export const Menu: React.FunctionComponent = () => {
-   
+
 
     const [containerRef, isVisible] = useScrollToNav({
         root: null,
@@ -20,31 +16,33 @@ export const Menu: React.FunctionComponent = () => {
 
     const MenuRef = useRef()
 
-    const scroll = (index : number) => {
+    const scroll = (index: number) => {
         //@ts-ignore
         MenuRef.current.handleClick(index)
     }
 
-    
-
-   
     return (
 
-        <div  id="page" className="container flex grid grid-rows-7 grid-cols-1 border-solid table-auto">
-            
-            
-      
-
-        <div className="sticky top-0 bg-white">{isVisible ? null : <ScrollCats scrollFC={scroll} />}</div>
+        <div id="page" className="container flex grid grid-rows-7 grid-cols-1 border-solid table-auto">
+            {/*@ts-ignore*/}
+            <Transition show={!isVisible}
+                enter="transition ease-out duration-1"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-1"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <div className="fixed top-0 bg-white"><ScrollCats scrollFC={scroll} /></div></Transition>
             <div>
-                {/*@ts-ignore */ }
+                {/*@ts-ignore */}
                 <div ref={containerRef} ><Head scrollFC={scroll} /></div>
-                <div id="menuComponent" className="flex-auto overflow-y-auto pb-96" ><MenuComponent ref={MenuRef}/></div>
+                <div id="menuComponent" className="flex-auto overflow-y-auto pb-96" ><MenuComponent ref={MenuRef} /></div>
             </div>
-            
-            
-            <footer onClick={() => {}}  className="flex-grow fixed bottom-0 w-full"><OrderButton /></footer>
+
+            <footer onClick={() => { }} className="flex-grow fixed bottom-0 w-full"><OrderButton /></footer>
 
         </div>
-    )}
+    )
+}
 
--- src/services/utilities.ts
+++ src/services/utilities.ts
@@ -1,7 +1,3 @@
-
-
-
-
 export const priceToLocal = (price: number) => {
     return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(price / 100)
 }
@@ -18,4 +14,3 @@ export const scrollTo = (element : HTMLDivElement) => {
     })
     */
 }
-    
