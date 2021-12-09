ea41b563977bcadb6c4433d414911f86bae4125c
Lars Flieger <a.purenetwork@gmail.com>
2021-12-09 17:21:23 +0100

    Add icons random (to have more relastic stage of the app)

--- src/components/MenuComponents/Categories.tsx
+++ src/components/MenuComponents/Categories.tsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useAppState } from '../../overmind'
+import { faHamburger, faAppleAlt, faBacon, faBreadSlice, faCheese, faFish, faPizzaSlice, faHotdog, faDrumstickBite } from "@fortawesome/free-solid-svg-icons"
 
 type PropTypes = {
     //scrolling Function gets passed through
@@ -10,7 +11,7 @@ type PropTypes = {
 export const Categories: React.FunctionComponent<PropTypes> = ({ scrollToRef }: PropTypes) => {
 
     const { menu } = useAppState().menu
-
+    const icons = [faHamburger, faAppleAlt, faBacon, faBreadSlice, faCheese, faFish, faPizzaSlice, faHotdog, faDrumstickBite];
 
     return (
         // All Categories
@@ -18,7 +19,7 @@ export const Categories: React.FunctionComponent<PropTypes> = ({ scrollToRef }: 
             {menu.categories.map((category, index) =>
                 <button onClick={() => scrollToRef(index)} key={category._id + "_button" + index} id={"categoryButton_" + index} className="text-red font-bold text-center m-1 h-16 w-20 shadow-md rounded-md overflow-hidden text-xs b-2" >
                     <div className="text-2xl ">
-                        <FontAwesomeIcon icon="hamburger" />
+                        <FontAwesomeIcon icon={icons[Math.floor(Math.random() * icons.length)]} />
                     </div>
                     {category.name}
                 </button>)}
