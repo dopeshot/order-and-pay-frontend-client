e730909809fc1dd53380a8b8459e336d4b93bebe
Ben Häußermann <ben.haussermann@gmail.com>
2022-01-08 10:08:52 +0100

    implement scrollable menuitem

--- src/components/MenuComponents/MenuItem.tsx
+++ src/components/MenuComponents/MenuItem.tsx
@@ -34,28 +34,30 @@ export const MenuItem: React.FunctionComponent<PropTypes> = ({ dish, menuItemOpe
     ))
 
     return (
-        <div id="menuItem" className=" overflow-y-auto w-full h-full left-0 fixed bottom-0" onClick={() => setMenuItemOpen(false)} >
-            <div className={`container mt-96 flex flex-col shadow-md rounded-md bg-white`} >
-                <div className="self-start flex flex-col w-full justify-between">
-                    <div className="self-start justify-between w-full">
-                        <div className="float-left font-bold">{dish.name}</div>
-                        <div className="float-right text-red font-bold">{priceToLocal(dish.price)}</div>
+        <div id="menuItem" className=" overflow-y-auto  h-full w-full left-0 fixed bottom-0 bgtrans no-scrollbar " onClick={() => setMenuItemOpen(false)} >
+            <div className={`container flex flex-col margin75P`} >
+                <div className={`margin75P bg-white shadow-md rounded-md  `}>
+                    <div className="self-start flex flex-col w-full justify-between">
+                        <div className="self-start justify-between w-full">
+                            <div className="float-left font-bold">{dish.name}</div>
+                            <div className="float-right text-red font-bold">{priceToLocal(dish.price)}</div>
+                        </div>
+                        <div className="self-start text-gray-400">{dish.description}</div>
                     </div>
-                    <div className="self-start text-gray-400">{dish.description}</div>
-                </div>
-                <p className="pt-2 font-bold">Allergien</p>
-                <div className="flex overflow-x-auto">
-                    {allergens}
-                </div>
-                <div className="pt-2">
-                    {choices}
-                </div>
-                <p className="pt-2 font-bold pb-2">Notiz an die Küche</p>
-                <div className="border rounded shadow h-24 flex justify-between items-stretch">
-                    <p className="pt-2 pl-2 text-gray-400">Platz für Wünsche...</p>
-                    <div className="h-full pt-2 pr-2 flex flex-col justify-between">
-                        <FontAwesomeIcon icon="hamburger" className="text-red self-end" />
-                        <p className="text-xs">edited: 69:69</p>
+                    <p className="pt-2 font-bold">Allergien</p>
+                    <div className="flex overflow-x-auto">
+                        {allergens}
+                    </div>
+                    <div className="pt-2">
+                        {choices}
+                    </div>
+                    <p className="pt-2 font-bold pb-2">Notiz an die Küche</p>
+                    <div id="notes" className="border rounded shadow mb-16 h-24 flex justify-between items-stretch">
+                        <p className="pt-2 pl-2 text-gray-400">Platz für Wünsche...</p>
+                        <div className="h-full pt-2 pr-2 flex flex-col justify-between">
+                            <FontAwesomeIcon icon="hamburger" className="text-red self-end" />
+                            <p className="text-xs">edited: 69:69</p>
+                        </div>
                     </div>
                 </div>
             </div>
