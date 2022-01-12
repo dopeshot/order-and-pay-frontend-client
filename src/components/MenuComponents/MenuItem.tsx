4e9a95fa7e6f3adba8e87c879f29e1fa7fb2ef96
Sarah Mauff <sm236@hdm-stuttgart.de>
2022-01-12 22:52:01 +0100

    few design changes

--- src/components/MenuComponents/MenuItem.tsx
+++ src/components/MenuComponents/MenuItem.tsx
@@ -16,11 +16,14 @@ export const MenuItem: React.FunctionComponent<PropTypes> = ({ dish, menuItemOpe
     const choices = dish.choices.filter(choice => choice.type == 'multi').map((choice, index) => (
         <div className="flex flex-col pt-2">
             <div className="self-start font-bold">{choice.name}</div>
-            <div className="flex justify-between">{choice.options.map((option) => (
-                <button>
-                    <div>{option.name}</div>
-                    <div>{priceToLocal(option.price)}</div>
-                </button>
+            <div className="flex flex-col">{choice.options.map((option) => (
+                <div className="flex items-center pl-3 pr-3">
+                    <input type="checkbox" className="form-checkbox text-red"></input>
+                    <div className="flex justify-between w-full pl-3">
+                        <div>{option.name}</div>
+                        <div>{priceToLocal(option.price)}</div>
+                    </div>
+                </div>
             ))}</div>
         </div>
 
@@ -40,7 +43,8 @@ export const MenuItem: React.FunctionComponent<PropTypes> = ({ dish, menuItemOpe
             <div className="bg-red opacity-50 inset-0 w-full h-full fixed" style={{ zIndex: -1 }} onClick={() => setMenuItemOpen(false)} />
             <div className="container flex flex-col margin75P">
                 <div className="w-full" style={{ height: "40rem" }} onClick={() => setMenuItemOpen(false)} />
-                <div className="bg-white shadow-md rounded-md pb-64 ">
+                <div className="bg-white shadow-md rounded-md pb-64 pl-3 pr-3 pt-3">
+                    <div className="flex flex-col"><FontAwesomeIcon icon="minus" className="text-gray-600 fa-2x self-center" /></div>
                     <div className="self-start flex flex-col w-full justify-between">
                         <div className="self-start justify-between w-full">
                             <div className="float-left font-bold">{dish.name}</div>
@@ -60,8 +64,7 @@ export const MenuItem: React.FunctionComponent<PropTypes> = ({ dish, menuItemOpe
                     <div id="notes" className="border rounded shadow mb-16 h-24 flex justify-between items-stretch">
                         <p className="pt-2 pl-2 text-gray-400">Platz für Wünsche...</p>
                         <div className="h-full pt-2 pr-2 flex flex-col justify-between">
-                            <FontAwesomeIcon icon="hamburger" className="text-red self-end" />
-                            <p className="text-xs">edited: 69:69</p>
+                            <FontAwesomeIcon icon="edit" className="text-red self-end" />
                         </div>
                     </div>
                 </div>
