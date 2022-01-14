71785d008a5fc27bdd61eda85da5cc3c11a4bcd6
Sarah Mauff <sm236@hdm-stuttgart.de>
2022-01-14 21:06:28 +0100

    add image to menu-item

--- src/components/MenuComponents/MenuItem.tsx
+++ src/components/MenuComponents/MenuItem.tsx
@@ -9,11 +9,9 @@ type PropTypes = {
     dish: Dish
     menuItemOpen: boolean
     setMenuItemOpen: (bool: boolean) => void
-
-
 }
-export const MenuItem: React.FunctionComponent<PropTypes> = ({ dish, menuItemOpen, setMenuItemOpen }: PropTypes) => {
 
+export const MenuItem: React.FunctionComponent<PropTypes> = ({ dish, menuItemOpen, setMenuItemOpen }: PropTypes) => {
 
     const [dropDownOpen, setdropDownOpen] = useState(false)
 
@@ -34,12 +32,10 @@ export const MenuItem: React.FunctionComponent<PropTypes> = ({ dish, menuItemOpe
                 ))}</div>
             </div>}
             {choice.type === "single" && <Dropdown choice={choice} dropDownOpen={dropDownOpen} setdropDownOpen={setdropDownOpen}></Dropdown>}
-        </div>))
-
-
+        </div>)
+    )
 
     const allergens = dish.allergens.map((allergen) => (
-
         <div className="m-3 flex flex-col items-center">
             <div className="h-7 w-7 bg-red text-center rounded-md">
                 <FontAwesomeIcon icon="hamburger" className="text-white h-full w-full" />
@@ -48,13 +44,17 @@ export const MenuItem: React.FunctionComponent<PropTypes> = ({ dish, menuItemOpe
         </div>
     ))
 
+
     return (
         <div id="menuItem" className="overflow-y-auto h-full w-full left-0 fixed bottom-0 bgtrans no-scrollbar" >
             <div className="bg-menu-bg bg-opacity-50 inset-0 w-full h-full fixed" style={{ zIndex: -1 }} onClick={() => setMenuItemOpen(false)} />
             <div className="container flex flex-col margin75P">
                 <div className="w-full" style={{ height: "40rem" }} onClick={() => setMenuItemOpen(false)} />
                 <div className="bg-white shadow-md rounded-md pb-64 pl-3 pr-3 pt-3">
-                    <div className="flex flex-col"><FontAwesomeIcon icon="minus" className="text-gray-600 fa-2x self-center" /></div>
+                    {dish.img !== "" && dish.img ?
+                        <div className=" w-full">
+                            <img src={dish.img}></img>
+                        </div> : <div className="flex flex-col"><FontAwesomeIcon icon="minus" className="text-gray-600 fa-2x self-center" /></div>}
                     <div className="self-start flex flex-col w-full justify-between">
                         <div className="self-start justify-between w-full">
                             <div className="float-left font-bold">{dish.name}</div>
@@ -71,13 +71,11 @@ export const MenuItem: React.FunctionComponent<PropTypes> = ({ dish, menuItemOpe
                     <div id="notes" className="border rounded shadow mb-16 h-24 flex justify-between items-stretch">
                         <p className="pt-2 pl-2 text-gray-400">Platz für Wünsche...</p>
                         <div className="h-full pt-2 pr-2 flex flex-col justify-between">
-                            <FontAwesomeIcon icon="edit" className="text-red self-end" />
+                            <button><FontAwesomeIcon icon="edit" className="text-red self-end" /></button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-
     )
 }
