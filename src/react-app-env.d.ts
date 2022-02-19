d12c13c9ca8a70eb2bfe4b49da8898d870ca81ed
Sarah Mauff <sm236@hdm-stuttgart.de>
2022-02-19 15:22:46 +0100

    add env

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+        REACT_APP_API_URL: string
+        DOCKER_PORT_IN: string
+        DOCKER_PORT_OUT: string
+        DOCKER_TEST_PORT_OUT: string
+        DOCKER_TEST_PORT_IN: string
+    }
+}
