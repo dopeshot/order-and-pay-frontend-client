29cee5101617b129e939240b0dfbb0de83b43890
Ben Häußermann <ben.haussermann@gmail.com>
2022-02-19 17:09:05 +0100

    fix instrumented files

--- instrumented/components/UIComponents/PaymentMethod.tsx
+++ instrumented/components/UIComponents/PaymentMethod.tsx
@@ -5,22 +5,19 @@ import React from "react";
 type PropTypes = {
     name: string,
     id: string,
-    icon: string
+    icon: string | IconDefinition
 }
 
-
-
 export const PaymentMethod: React.FunctionComponent<PropTypes> = ({ name, icon, id }: PropTypes) => {
-
 
     return (
 
         <div className="flex items-center pl-3 pr-3 pt-3">
-            <div className="flex w-full pl-3 text-xl">
+            <div className="flex w-full pl-3 text-xl items-center">
                 <FontAwesomeIcon icon={icon as IconProp} className='text-red' />
                 <p className='pl-3'>{name}</p>
             </div>
-            <input type="radio" name="checked" value={id} />
+            <input type="radio" name="checked" value={id} defaultChecked={true} />
         </div>
 
     )
