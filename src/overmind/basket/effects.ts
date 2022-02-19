a5a6021c28c450a30397f3c73095c88282f2f2ac
Lea Dannecker <leadannecker@googlemail.com>
2022-02-19 15:48:36 +0100

    Change /orders enpoint for get request to /order

--- src/overmind/basket/effects.ts
+++ src/overmind/basket/effects.ts
@@ -2,13 +2,9 @@ import { request } from "../../services/axios";
 import { SendBasket } from "./state";
 
 
-
-
 export const getPrice = (): Promise<any> => {
     return request.get<Number>('/menu') // /menus/:id/refs
 }
 
-//Endpunkt mit Max abklären   
-export const sendOrder = (basket: SendBasket) => console.log("effect:", JSON.stringify(basket))
 
-//export const sendOrder = (basket: Basket) => request.post<any>('/orders', basket)
+export const sendOrder = (basket: SendBasket) => request.post<any>('/order', basket)
