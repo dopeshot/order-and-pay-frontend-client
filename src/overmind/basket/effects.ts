785d0445eceaffd6f82cf48709c43b1dff961542
coffemakingtoaster <mh340@hdm-stuttgart.de>
2022-02-20 14:44:48 +0100

    send data to server rather than logging it

--- src/overmind/basket/effects.ts
+++ src/overmind/basket/effects.ts
@@ -2,8 +2,9 @@ import { request } from "../../services/axios";
 import { SendBasket } from "./state";
 
 export const getPrice = (): Promise<any> => {
-    return request.get<Number>('/menu') // /menus/:id/refs
-}
+  return request.get<Number>("/menu"); // /menus/:id/refs
+};
 
-export const sendOrder = (basket: SendBasket) => console.log("effect:", JSON.stringify(basket))
-//export const sendOrder = (basket: SendBasket) => request.post<any>('/order', basket)
+//export const sendOrder = (basket: SendBasket) => console.log("effect:", JSON.stringify(basket))
+export const sendOrder = (basket: SendBasket) =>
+  request.post<any>("/order", basket);
